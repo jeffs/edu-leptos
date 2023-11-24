5bc24664509e1eb092d2858b4095f2d73a4901da
Jeff Schwab <jeff@unbuggy.com>
2023-11-23 21:25:14 -0500

    Define component ProgressBar

--- leptos-tutorial/src/app.rs
+++ leptos-tutorial/src/app.rs
@@ -1,6 +1,8 @@
-use leptos::{component, create_signal, logging::log, view, IntoView, SignalUpdate, SignalWith};
+use leptos::{
+    component, create_signal, logging::log, view, IntoView, Signal, SignalUpdate, SignalWith,
+};
 
-const GOAL: u32 = 8;
+const GOAL: i32 = 8;
 
 fn next_random(seed: u32) -> u32 {
     // https://stackoverflow.com/a/3062783/3116635
@@ -23,15 +25,31 @@ impl Position {
 }
 
 #[component]
+fn ProgressBar(
+    #[prop(default = GOAL as u16)] max: u16,
+    #[prop(into)] progress: Signal<i32>,
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
+    }
+}
+
+#[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
     let (pos, set_pos) = create_signal(Position::default());
+
+    let double_count = move || count() * 2;
 
     view! {
         <main
             class:won={move || count() >= GOAL}
         >
-            <progress max=GOAL value=count />
+            <ProgressBar progress=count />
+            <ProgressBar progress=Signal::derive(double_count) />
             <button
                 class:red=move || count() % 2 == 1
                 style:top={move || pos.with(|r| format!("{}vh", r.top))}
@@ -42,8 +60,7 @@ pub fn App() -> impl IntoView {
                     pos.with(|p| log!("{p:?}"));
                 }
             >
-                "Click me: "
-                {count}
+                "Click me: " {count}
             </button>
             <footer>{move || pos.with(|r| format!("{r:?}"))}</footer>
         </main>
