604d2f3c6df4b5bb46c61b28eabf675b324dfe28
Jeff Schwab <jeff@unbuggy.com>
2023-11-23 22:57:21 -0500

    Add DynamicList

--- leptos-tutorial/src/app.rs
+++ leptos-tutorial/src/app.rs
@@ -1,5 +1,6 @@
 use leptos::{
-    component, create_signal, logging::log, view, CollectView, IntoView, SignalUpdate, SignalWith,
+    component, create_signal, logging::log, view, For, IntoView, ReadSignal, SignalUpdate,
+    SignalWith, WriteSignal,
 };
 
 const GOAL: i32 = 8;
@@ -42,34 +43,71 @@ fn ProgressBar<F: Fn() -> i32 + 'static>(
 }
 
 #[component]
+fn DynamicList() -> impl IntoView {
+    let initial_counters: Vec<_> = (1..=5).map(|id| (id, create_signal(0))).collect();
+    let mut last_counter_id = initial_counters.len();
+    let (counters, set_counters) = create_signal(initial_counters);
+    let (sum, set_sum) = create_signal(0);
+
+    let append = move |_| {
+        set_counters.update(|v| {
+            last_counter_id += 1;
+            v.push((last_counter_id, create_signal(0)));
+        });
+    };
+
+    let remove = move |id| {
+        move |_| {
+            set_counters.update(|counters_| counters_.retain(|(id_, _)| *id_ != id));
+            set_sum.update(|sum_| {
+                *sum_ = counters()
+                    .into_iter()
+                    .map(|(_, (counter, _))| counter())
+                    .sum();
+            });
+        }
+    };
+
+    let render =
+        move |(id, (counter, set_counter)): (usize, (ReadSignal<i32>, WriteSignal<i32>))| {
+            let increment = move |_| {
+                set_counter.update(|n| *n += 1);
+                set_sum.update(|sum_| *sum_ += 1);
+            };
+            view! {
+                <li value={id}>
+                    <button on:click=remove(id)>-</button>
+                    <button on:click=increment>{counter}</button>
+                </li>
+            }
+        };
+
+    view! {
+        <button class="plus" on:click=append>+</button>
+        <ol>
+            <For
+                each=counters
+                key=|counter| counter.0
+                children=render
+            />
+        </ol>
+        {sum}
+    }
+}
+
+#[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
     let (pos, set_pos) = create_signal(Position::default());
 
     let double_count = move || count() * 2;
 
-    let buttons = (0..5)
-        .map(create_signal)
-        .map(|(count, set_count)| {
-            view! {
-                <li>
-                    <button
-                        on:click=move |_| set_count.update(|n| *n += 1)
-                    >
-                        {count}
-                    </button>
-                </li>
-            }
-        })
-        .collect_view();
-
     view! {
-        <main
-            class:won={move || count() >= GOAL}
-        >
+        <main class:won={move || count() >= GOAL}>
             <ProgressBar progress=count />
             <ProgressBar progress=double_count />
             <button
+                class="tile"
                 class:red=move || count() % 2 == 1
                 style:top={move || pos.with(|r| format!("{}vh", r.top))}
                 style:left={move || pos.with(|r| format!("{}vw", r.left))}
@@ -81,7 +119,7 @@ pub fn App() -> impl IntoView {
             >
                 "Click me: " {count}
             </button>
-            <ul>{buttons}</ul>
+            <DynamicList />
             <footer>{move || pos.with(|r| format!("{r:?}"))}</footer>
         </main>
     }
