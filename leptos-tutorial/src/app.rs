90aba0c0ef30f42e15598c98f0c61c7464701ec2
Jeff Schwab <jeff@unbuggy.com>
2023-12-31 23:22:31 -0500

    Add a few walls at silly locations

--- leptos-tutorial/src/app.rs
+++ leptos-tutorial/src/app.rs
@@ -1,4 +1,6 @@
-use leptos::{component, create_signal, ev::KeyboardEvent, view, IntoView, SignalUpdate};
+use leptos::{
+    component, create_signal, ev::KeyboardEvent, view, CollectView, IntoView, SignalUpdate,
+};
 
 const SCALE: u32 = 10;
 
@@ -8,21 +10,44 @@ const DUNGEON_HEIGHT: u32 = 40; // m
 const PLAYER_WIDTH: u32 = 1; // m
 const PLAYER_HEIGHT: u32 = 2; // m
 
+const WALL_WIDTH: u32 = 1; // m
+const WALL_HEIGHT: u32 = 2; // m
+
 fn do_both<F: FnOnce(), G: FnOnce()>(f: F, g: G) {
     f();
     g();
 }
 
 #[component]
+
+fn Wall(x: u32, y: u32) -> impl IntoView {
+    view! {
+        <div
+           class="entity wall"
+           style:translate=move || format!("{}px {}px", x * SCALE, y * SCALE)
+           style:width=format!("{}px", WALL_WIDTH * SCALE)
+           style:height=format!("{}px", WALL_HEIGHT * SCALE)
+
+        />
+    }
+}
+
+#[component]
 pub fn App() -> impl IntoView {
+    // Signals
     let (x, set_x) = create_signal((DUNGEON_WIDTH - PLAYER_WIDTH) / 2);
     let (y, set_y) = create_signal((DUNGEON_HEIGHT - PLAYER_HEIGHT) / 2);
 
+    // Walls (x, y)
+    //
+    // TODO: Remove walls if the player bumps into them.
+    let walls = [(8, 6), (7, 5), (3, 0)];
+
+    // Handlers
     let go_north = move || set_y.update(|y| *y -= 1);
     let go_east = move || set_x.update(|x| *x += 1);
     let go_south = move || set_y.update(|y| *y += 1);
     let go_west = move || set_x.update(|x| *x -= 1);
-
     let handle_keypress = move |ev: KeyboardEvent| match ev.key().as_str() {
         "h" => go_west(),
         "j" => go_south(),
@@ -40,23 +65,21 @@ pub fn App() -> impl IntoView {
             style:border="2px solid red"
             style:width=format!("{}px", DUNGEON_WIDTH * SCALE)
             style:height=format!("{}px", DUNGEON_HEIGHT * SCALE)
-
-            tabindex=0
-            autofocus=true
-            style:outline="none"
-            on:keypress=handle_keypress
         >
+            // Walls
+            {walls.into_iter().map(|(x, y)| view! { <Wall x y /> }).collect_view()}
+
+            // Player
             <div
-                class="mob"
-
-                style:position="absolute"
+                class="entity mob"
                 style:translate=move || format!("{}px {}px", x() * SCALE, y() * SCALE)
-
-                style:border="2px solid silver"
-                style:box-shadow="2px 2px 2px black"
-
                 style:width=format!("{}px", PLAYER_WIDTH * SCALE)
                 style:height=format!("{}px", PLAYER_HEIGHT * SCALE)
+
+                tabindex=0
+                autofocus=true
+                // style:outline="none"
+                on:keypress=handle_keypress
             />
 
             <p style:position="fixed" style:bottom=0>{x} {y}</p>
